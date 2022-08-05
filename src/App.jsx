412ccd3a0c6822281165a816be9f362e275aabd5
Pulatof <niyozbekpulatov@gmail.com>
2022-08-05 14:29:13 +0500

    project setups completed

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
